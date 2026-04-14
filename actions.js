e0d5247fdd264c54987182115d2bd8dc26c495a2
Allyza <aftwacan@gmail.com>
2026-04-14 11:32:31 +0800

    Added Projects: Moved Images to a folder: Added in Skills Section

--- actions.js
+++ actions.js
@@ -1,6 +1,6 @@
 const img = document.querySelector('.pfp');
-const img1 = 'pfp.png'
-const img2 = 'pfp2.gif'
+const img1 = 'images/pfp.png'
+const img2 = 'images/pfp2.gif'
 
 img.addEventListener('click', () => {
     img.src = img2 + "?t=" + Date.now();
