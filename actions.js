0348f077c001554e09ca0e8f56dfd3f37d77d121
Allyza <aftwacan@gmail.com>
2026-04-14 14:28:41 +0800

    Try modified js

--- actions.js
+++ actions.js
@@ -3,11 +3,11 @@ const img1 = 'images/pfp.png'
 const img2 = 'images/pfp2.gif'
 
 img.addEventListener('click', () => {
-    img.src = img2 + "?t=" + Date.now();
+    img.src = img2
 
 });
 
 img.addEventListener('mouseout', () => {
-    img.src = img1 + "?t=" + Date.now();
+    img.src = img1
 });
 
