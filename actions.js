f5e5963f11a1ad0bb48010df17ec51fb6517fcdd
Allyza <aftwacan@gmail.com>
2026-04-14 14:32:29 +0800

    Change from Click to Mouseover

--- actions.js
+++ actions.js
@@ -2,7 +2,7 @@ const img = document.querySelector('.pfp');
 const img1 = 'images/pfp.png'
 const img2 = 'images/pfp2.gif'
 
-img.addEventListener('click', () => {
+img.addEventListener('mouseover', () => {
     img.src = img2
 
 });
